1f54cb28677c0a53f9415a856d8dbbce198757db
ReenaH <Reena_Hunjan@yahoo.com>
2022-06-17 18:26:51 -0400

    create coordinate array and station cards with station details

--- assets/script.js
+++ assets/script.js
@@ -2,42 +2,89 @@
 
 var inputEl = document.querySelector(".text-input");
 var searchButtonEl = document.querySelector("#search-station");
+var displayStationEl = document.querySelector("#station_details");
+
+//An array of lat and long coordinates to be used by the map api
+var mapArray = [];
  
 
 // Get zipcode or postal code
 var zipOrpostal = function(){
 
-    fetch("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=all&location=New+York&limit=10&radius=500&api_key=SwEWgqT1Snyw7Pp3sxyGmCNlQ5okceAAFKUr5PTU")
+    fetch("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=all&location=New+York&limit=5&radius=500&api_key=SwEWgqT1Snyw7Pp3sxyGmCNlQ5okceAAFKUr5PTU")
     .then(function(response){
         response.json().then(function(data){
             console.log(data);
-            
-            // get api data
+
+            //populate the mapArray with locations
+            for (var i=0; i <5; i++){
+                
+            // get location data
+            var mapCoordinates = {
+                lat: data['fuel_stations'][i]['latitude'],
+                lon: data['fuel_stations'][i]['latitude'],
+            };
+            //create an array of coordinates for the map
+            mapArray.push(mapCoordinates);
+
+            //grab station details
+
             var station = {
-                lon: data['longitude'],
-                lat: data['latitude'],
-                access: data['fuel_stations'][0]['access_code'],
-                accessHours: data['fuel_stations'][0]['access_days_time'],
-                distance: data['fuel_stations'][0]['distance_km'],
-                ev_connector_types: data['fuel_stations'][0]['ev_connector_types'],
-                ev_network: data['fuel_stations'][0]['ev_network'],
-                address: data['fuel_stations'][0]['station_name'],
-                phone: data['fuel_stations'][0]['station_phone'],
-                zip: data['fuel_stations'][0]['zip'], 
+                ev_network: data['fuel_stations'][i]['ev_network'],
+                ev_connector_types: data['fuel_stations'][i]['ev_connector_types'],
+                distance: data['fuel_stations'][i]['distance_km'],
+                address: data['fuel_stations'][i]['station_name'],
+                zip: data['fuel_stations'][i]['zip'], 
+                phone: data['fuel_stations'][i]['station_phone'],
+                access: data['fuel_stations'][i]['access_code'],
+                accessHours: data['fuel_stations'][i]['access_days_time']
+                }
+
+               console.log(station.ev_network, station.ev_connector_types, station.distance,
+                station.address, station.zip, station.phone, station.access, station.accessHours);
+
+                //create elements and add content
+                var stationCard = document.createElement('div')
+                var stationDetailNetwork = document.createElement('p');
+                stationDetailNetwork.innerHTML = `EV Network: ${station.ev_network}`;
+
+                var stationDetailConnectorType = document.createElement('p');
+                stationDetailConnectorType.innerHTML = `EV Connector Type: ${station.ev_connector_types}`;
+
+                var stationDetailDistance = document.createElement('p');
+                stationDetailDistance.innerHTML = `Station is ${station.distance}km away`;
+
+                 var stationDetailAddress = document.createElement('p');
+                stationDetailAddress.innerHTML = `Station address: ${station.address}`;
+
+                 var stationDetailZip = document.createElement('p');
+                stationDetailZip.innerHTML = `Zip: ${station.zip}`;
+
+                 var stationDetailPhone = document.createElement('p');
+                stationDetailPhone.innerHTML = `Phone: ${station.phone}`;
+
+                 var stationDetailAccess = document.createElement('p');
+                stationDetailAccess.innerHTML = `Access: ${station.access}`;
+
+                var stationDetailAccessHours = document.createElement('p');
+                stationDetailAccessHours.innerHTML = `Access Hours: ${station.access}`;
+
+                //add details to card
+                stationCard.appendChild(stationDetailNetwork);
+                stationCard.appendChild(stationDetailConnectorType);
+                stationCard.appendChild(stationDetailDistance);
+                stationCard.appendChild(stationDetailAddress);
+                stationCard.appendChild(stationDetailZip);
+                stationCard.appendChild(stationDetailPhone);
+                stationCard.appendChild(stationDetailAccess);
+                stationCard.appendChild(stationDetailAccessHours);
+               
+               
+                //add card to station details div
+                displayStationEl.appendChild(stationCard);
             }
 
-            //check data
-            console.log(station.lon, station.lat, station.access, station.accessHours,
-                station.distance, station.ev_connector_types, station.ev_network, station.address,
-                station.phone, station.zip);
           
-
-            //create elements
-            var stationCard = document.createElement("div");
-            var stationDetail = document.createElement("p");
-
-         
-
          
         })
     })
@@ -45,8 +92,10 @@ var zipOrpostal = function(){
 
 // Click search button
 searchButtonEl.addEventListener("click", function(){
+    mapArray = [];
     zipOrpostal();
     searchButtonEl.innerHTML = "";
+    
 });
  
  
